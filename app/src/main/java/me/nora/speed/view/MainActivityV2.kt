8c11c2ebcda5fc1fe394b185934a3bc066d8cbce
norazlan <norazlan.yasin@sapura.com.my>
2024-10-23 16:07:56 +0800

    stuff

--- app/src/main/java/me/nora/speed/view/MainActivityV2.kt
+++ app/src/main/java/me/nora/speed/view/MainActivityV2.kt
@@ -1,24 +1,19 @@
 package me.nora.speed.view
 
-import me.nora.speed.databinding.ActivityMainBinding
+import android.Manifest
 import android.content.Context
 import android.content.pm.PackageManager
-import android.location.Location
-import android.Manifest
 import android.content.res.Configuration
-import android.graphics.Color
+import android.location.Location
 import android.location.LocationListener
 import android.location.LocationManager
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.TypedValue
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
-import com.github.mikephil.charting.charts.LineChart
-import com.github.mikephil.charting.components.Description
-import com.github.mikephil.charting.components.XAxis
-import com.github.mikephil.charting.data.Entry
-import com.github.mikephil.charting.data.LineData
-import com.github.mikephil.charting.data.LineDataSet
-import com.github.mikephil.charting.formatter.ValueFormatter
+import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.WindowInsetsControllerCompat
 import me.nora.speed.databinding.ActivityMainV2Binding
 import java.util.*
 
@@ -26,9 +21,7 @@ class MainActivityV2 : AppCompatActivity(), LocationListener {
 
     private lateinit var binding: ActivityMainV2Binding
     private lateinit var locationManager: LocationManager
-
-    private var firstCount = false
-    private var startTime: Long = 0
+    private var isFullscreen = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -36,6 +29,64 @@ class MainActivityV2 : AppCompatActivity(), LocationListener {
         setContentView(binding.root)
         locationManager = getSystemService(Context.LOCATION_SERVICE) as LocationManager
         checkLocationPermission()
+
+        binding.ivFlip.setOnClickListener {
+            setFlip()
+        }
+
+        binding.ivFull.setOnClickListener {
+            if (isFullscreen) {
+                exitFullscreen()
+            } else {
+                enterFullscreen()
+            }
+            isFullscreen = !isFullscreen
+        }
+
+        binding.ivReset.setOnClickListener {
+            binding.tvSpeed.setTextSize(TypedValue.COMPLEX_UNIT_SP, 130f)
+            binding.tvSpeedBg.setTextSize(TypedValue.COMPLEX_UNIT_SP, 130f)
+            binding.tvUnit.setTextSize(TypedValue.COMPLEX_UNIT_SP, 30f)
+        }
+    }
+
+    private fun setFlip() {
+        if (binding.tvUnit.scaleX != -1F) {
+            binding.tvUnit.scaleX = -1F
+            binding.tvSpeed.scaleX = -1F
+        } else {
+            binding.tvUnit.scaleX = 1F
+            binding.tvSpeed.scaleX = 1F
+        }
+    }
+
+    override fun onConfigurationChanged(newConfig: Configuration) {
+        super.onConfigurationChanged(newConfig)
+        if (newConfig.orientation == Configuration.ORIENTATION_LANDSCAPE) {
+            binding.tvSpeed.setTextSize(TypedValue.COMPLEX_UNIT_SP, 170f)
+            binding.tvSpeedBg.setTextSize(TypedValue.COMPLEX_UNIT_SP, 170f)
+            binding.tvUnit.setTextSize(TypedValue.COMPLEX_UNIT_SP, 35f)
+        } else if (newConfig.orientation == Configuration.ORIENTATION_PORTRAIT) {
+            binding.tvSpeed.setTextSize(TypedValue.COMPLEX_UNIT_SP, 130f)
+            binding.tvSpeedBg.setTextSize(TypedValue.COMPLEX_UNIT_SP, 130f)
+            binding.tvUnit.setTextSize(TypedValue.COMPLEX_UNIT_SP, 30f)
+        }
+    }
+
+    private fun enterFullscreen() {
+        val decorView: View = window.decorView
+        val windowInsetsController = WindowInsetsControllerCompat(window, decorView)
+
+        windowInsetsController.hide(WindowInsetsCompat.Type.systemBars())
+        windowInsetsController.systemBarsBehavior =
+            WindowInsetsControllerCompat.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
+    }
+
+    private fun exitFullscreen() {
+        val decorView: View = window.decorView
+        val windowInsetsController = WindowInsetsControllerCompat(window, decorView)
+
+        windowInsetsController.show(WindowInsetsCompat.Type.systemBars())
     }
 
     private fun checkLocationPermission() {
@@ -50,12 +101,7 @@ class MainActivityV2 : AppCompatActivity(), LocationListener {
 
     override fun onLocationChanged(location: Location) {
         val speed = location.speed * 3.6f // convert m/s to km/h
-        binding.speedText.text = String.format(Locale.getDefault(), "%.0f", speed)
-
-        if(!firstCount) {
-            firstCount = true
-            startTime = System.currentTimeMillis()
-        }
+        binding.tvSpeed.text = String.format(Locale.getDefault(), "%.0f", speed)
     }
 
     override fun onStatusChanged(provider: String?, status: Int, extras: Bundle?) {}
